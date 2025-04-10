bb01209fb126bca0f24a03aa188f7b6d53462740
1i-2022-Kacper-24 <1i.2022.Kacper.24@gmail.com>
2025-04-10 12:09:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const paragraph = document.querySelector('p')
-const textColorInput = document.querySelector('[type="color"]')
-const button = document.querySelector('button')
+const paragraph = document.querySelectorAll('p')
 
-button.addEventListener('click', function(){
-    paragraph.style.color = textColorInput.value
+const button1 = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
+const button3 = document.querySelector('#button3')
+
+const textColorInput = document.querySelector('#textColorInput')
+const textSizeInput = document.querySelector('#textSizeInput')
+const backgroundColorInput = document.querySelector('#backgroundColorInput')
+
+button1.addEventListener('click', function(){
+    paragraph.forEach(paragraph => {
+    paragraph.style.color = textColorInput.value})
 })
+button2.addEventListener('click', function(){
+    paragraph.forEach(paragraph => {
+    paragraph.style.fontSize = `${textSizeInput.value}px`})
+})
+button3.addEventListener('click', function() {
+    paragraph.forEach(paragraph => {
+        paragraph.style.backgroundColor = backgroundColorInput.value;
+    });
+});
